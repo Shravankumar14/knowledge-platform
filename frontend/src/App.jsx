4f31531a0dbfbae07ba89601ecefb21673caf140
ShravanKumarDegala <shravankumardegala777@gmail.com>
2026-09-14 21:12:25 +0530

    Add React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CourseList from './pages/CourseList';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CourseList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
